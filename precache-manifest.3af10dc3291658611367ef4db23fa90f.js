a9f3da09c047155c82d2b06414737ef816254c7d
Victor <vctrd3@gmail.com>
2020-07-06 17:53:42 -0400

    Updates

---
+++ precache-manifest.3af10dc3291658611367ef4db23fa90f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa43bce37f36b750ba80f73001b8b739",
+    "url": "/retireup/index.html"
+  },
+  {
+    "revision": "0dc699f59a3f4fdfff41",
+    "url": "/retireup/static/css/2.ed590494.chunk.css"
+  },
+  {
+    "revision": "b73fd49c88fb5b407722",
+    "url": "/retireup/static/css/main.99607ad0.chunk.css"
+  },
+  {
+    "revision": "0dc699f59a3f4fdfff41",
+    "url": "/retireup/static/js/2.604a3f80.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/retireup/static/js/2.604a3f80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b73fd49c88fb5b407722",
+    "url": "/retireup/static/js/main.7941f01d.chunk.js"
+  },
+  {
+    "revision": "81ba3bb18ec696001b6c",
+    "url": "/retireup/static/js/runtime-main.f74d6ad3.js"
+  }
+]);
